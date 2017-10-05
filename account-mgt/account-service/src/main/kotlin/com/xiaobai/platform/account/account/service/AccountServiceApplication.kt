9821d66c260cddfcf13c6ef941f8d5e6505e3982
xiaobai <304139489@qq.com>
2017-10-05 21:07:34 +0800

    init

---
+++ account-mgt/account-service/src/main/kotlin/com/xiaobai/platform/account/account/service/AccountServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.xiaobai.platform.account.account.service
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AccountServiceApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(AccountServiceApplication::class.java, *args)
+}
